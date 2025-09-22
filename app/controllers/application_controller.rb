b300b2d9ea2d6c3c983b445cf58ff09faf8a15da
hgkim7 <hgkim2738@gmail.com>
2025-09-22 19:20:16 +0900

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   def hello
-    render html: "Ciao, mondo!¡"
+    render html: "Ciao, mondo!"
   end
   def goodbye
     render html: "Goodbye, world!"
